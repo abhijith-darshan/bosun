9a562de1028584739723cacb3f2f2d62f401b703
abhijith-darshan <abhijith.ravindra@sap.com>
2024-12-29 03:12:11 +0100

    (chore): setup wails app

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bosun/pkg"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:dashboards/dist/apps/bosun-ui
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := pkg.NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "bosun",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.Startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
